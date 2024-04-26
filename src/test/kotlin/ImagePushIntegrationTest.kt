27fab1fe4bdc9813d88cd9631c01c2d2560da106
Joshua Harding <josh@statewidesoftware.com>
2024-04-26 18:06:18 -0400

    added broken test

--- src/test/kotlin/ImagePushIntegrationTest.kt
+++ src/test/kotlin/ImagePushIntegrationTest.kt
@@ -1,5 +1,7 @@
 import blobstore.Blobstore
+import com.google.common.base.CharMatcher.any
 import io.javalin.testtools.JavalinTest
+import io.mockk.every
 import io.mockk.mockk
 import mu.KotlinLogging
 import kotlin.test.Test
@@ -9,12 +11,26 @@ class ImagePushIntegrationTest {
 
     val klogger = KotlinLogging.logger {}
     val mockBlobStore = mockk<Blobstore>()
+
+    init {
+        every { mockBlobStore.addBlob(any(), any(), any()) } returns 202
+    }
+
     private val javalinApp = RegistryServerApp(logger = klogger, mockBlobStore).javalinApp()
+
     @Test
-    fun testImagePush() {
+    fun testHelloWorld() {
         JavalinTest.test(javalinApp) { server, client ->
             val response = client.get("/")
             assertEquals("Hello World", response.body?.string())
+        }
+    }
+
+    @Test
+    fun testImagePush() {
+        JavalinTest.test(javalinApp) { server, client ->
+            val response = client.post("/v2/test/blobs/uploads/")
+            assertEquals(202, response.code)
         }
     }
 }
