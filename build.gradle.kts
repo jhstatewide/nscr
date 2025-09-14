f47df31b5e2465f953b0757ea5f0a17028e6c60d
Joshua Harding <josh@statewidesoftware.com>
2025-09-14 14:43:55 -0400

    upgraded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.0.21"
     idea
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 group = "com.statewidesoftware"
@@ -16,24 +17,24 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    implementation("io.javalin:javalin:6.1.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+    implementation("io.javalin:javalin:6.7.0")
     // get the javalin test tools as well
-    testImplementation("io.javalin:javalin-testtools:6.1.3")
-    implementation("com.h2database:h2:2.2.222")
-    implementation("org.jdbi", "jdbi3-core", "3.8.2")
-    implementation("org.jdbi", "jdbi3-kotlin", "3.8.2")
-    implementation("org.jdbi", "jdbi3-kotlin-sqlobject", "3.8.2")
-    api("com.google.code.gson:gson:2.8.9")
-    implementation("com.github.docker-java:docker-java-core:3.2.12")
-    implementation("com.github.docker-java:docker-java-transport-httpclient5:3.2.12")
-    implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("ch.qos.logback:logback-core:1.3.12")
+    testImplementation("io.javalin:javalin-testtools:6.7.0")
+    implementation("com.h2database:h2:2.3.232")
+    implementation("org.jdbi", "jdbi3-core", "3.49.5")
+    implementation("org.jdbi", "jdbi3-kotlin", "3.49.5")
+    implementation("org.jdbi", "jdbi3-kotlin-sqlobject", "3.49.5")
+    api("com.google.code.gson:gson:2.13.2")
+    implementation("com.github.docker-java:docker-java-core:3.6.0")
+    implementation("com.github.docker-java:docker-java-transport-httpclient5:3.6.0")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("ch.qos.logback:logback-core:1.5.18")
     implementation("io.github.microutils:kotlin-logging-jvm:2.1.20")
     // pull in mockk
-    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("io.mockk:mockk:1.14.5")
 }
 
 tasks.test {
@@ -46,7 +47,9 @@ tasks.withType<JavaCompile> {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "17"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 idea {
@@ -57,8 +60,8 @@ idea {
 }
 
 val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    freeCompilerArgs = listOf("-Xinline-classes")
+compileKotlin.compilerOptions {
+    freeCompilerArgs.add("-Xinline-classes")
 }
 
 application {
